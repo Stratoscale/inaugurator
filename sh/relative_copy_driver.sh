60128f0ea93578f089b7a5f78a406b37defd4f95
Shlomi Matichin <shlomi@stratoscale.com>
2014-06-29 21:18:55 +0300

    Bugfix: relative_copy_driver did not use correct kernel version in all mod* commands

--- sh/relative_copy_driver.sh
+++ sh/relative_copy_driver.sh
@@ -5,9 +5,10 @@
 set -e
 mkdir `dirname $INSMOD_SCRIPT` 2>/dev/null || true
 modprobe --show-depends $1 --set-version=$KERNEL_UNAME_R >> $INSMOD_SCRIPT
-for ko in `modprobe --show-depends $1 | sed 's/insmod //'`; do
+for ko in `modprobe --show-depends $1 --set-version=$KERNEL_UNAME_R | sed 's/insmod //'`; do
     sh/relative_copy_glob.sh $ko
 done
-for firmware in `modinfo --field firmware $1`; do
+modinfo --field firmware $1 --set-version=$KERNEL_UNAME_R >& /dev/null
+for firmware in `modinfo --field firmware $1 --set-version=$KERNEL_UNAME_R`; do
     sh/relative_copy_glob.sh /lib/firmware/$firmware
 done
