e6e15aeaa2917ddf61f764b5dcaa7d49197b3937
Shlomi Matichin <shlomi@stratoscale.com>
2014-10-22 10:19:42 +0300

    Fix build bug: build now fails if unable to copy an executable

--- sh/relative_copy_glob.sh
+++ sh/relative_copy_glob.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 #copy into the initrd with creating the parent directory first
 set -e
+at_least_one=0
+for path in $@; do
+    if [ ! -e "$path" ]; then
+        echo "$path does not exist"
+        exit 1
+    fi
+    at_least_one=1
+done
+if [ "$at_least_one" == "0" ]; then
+    echo "Zero parameters provided"
+    exit 1
+fi
 directory=`dirname $@ | head -1`
 mkdir -p $DEST/$directory 2>/dev/null
 for path in `find $@ -maxdepth 0 -and -not -type d`; do
