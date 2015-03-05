281d005795ad578e87f8a6d7b914e24e3e161678
Shlomi Matichin <shlomi@stratoscale.com>
2015-03-05 12:54:16 +0000

    Refactored main

---
+++ inaugurator/ceremony.py
@@ -0,0 +1,194 @@
+from inaugurator import partitiontable
+from inaugurator import targetdevice
+from inaugurator import mount
+from inaugurator import sh
+from inaugurator import network
+from inaugurator import loadkernel
+from inaugurator import fstab
+from inaugurator import passwd
+from inaugurator import osmose
+from inaugurator import osmosiscleanup
+from inaugurator import talktoserver
+from inaugurator import grub
+from inaugurator import diskonkey
+from inaugurator import udev
+from inaugurator import download
+from inaugurator import etclabelfile
+from inaugurator import lvmetad
+import os
+import time
+import logging
+
+
+class Ceremony:
+    def __init__(self, args):
+        """
+        args is a 'namespace' - an object, or maybe a bunch. The following members are required:
+        inauguratorClearDisk - True will cause the disk to be erase even if partition layout is ok
+        inauguratorSource - 'network', 'DOK' (Disk On Key), 'local' - select from where the label
+                            should be osmosed. 'local' means the label is already in the local object
+                            store, and is used in upgrades.
+        inauguratorServerAMQPURL - the rabbitmq AMQP url to report status to. Can be 'None'. If used,
+                                   the label itself is expected to come from a rabbitmq message.
+        inauguratorMyIDForServer - the unique ID for this station, used for status reporting.
+        inauguratorNetworkLabel - the label to use, in 'network' mode, if inauguratorServerAMQPURL was
+                                  not specified
+        inauguratorOsmosisObjectStores - the object store chain used when invoking osmosis (see osmosis
+                                         documentation
+        inauguratorUseNICWithMAC - use this specific NIC, with this specific MAC address
+        inauguratorIPAddress - the IP address to configure to that NIC
+        inauguratorNetmask
+        inauguratorGateway
+        inauguratorChangeRootPassword - change the password in /etc/shadow to this
+        inauguratorWithLocalObjectStore - use /var/lib/osmosis local object store as first tier in chain.
+        inauguratorPassthrough - pass parameters to the kexeced kernel. Reminder: kexeced kernel are
+                                 more vunerable to crashing, using this as the only channel of communication
+                                 is risky
+        inauguratorDownload - http get this file into a specific location, right before kexecing.
+        inauguratorIgnoreDirs - ignore the following locations on disk, in the osmosis process. This is
+                                usedful for upgrades - to keep the current configuration somewhere.
+        """
+        self._args = args
+        self._talkToServer = None
+        self._assertArgsSane()
+
+    def ceremony(self):
+        before = time.time()
+        self._makeSureDiskIsMountable()
+        with self._mountOp.mountRoot() as destination:
+            self._etcLabelFile = etclabelfile.EtcLabelFile(destination)
+            self._doOsmosisFromSource(destination)
+            logging.info("Osmosis complete")
+            self._createBootAndInstallGrub(destination)
+            logging.info("Boot sync complete")
+            self._configureETC(destination)
+            self._loadKernelForKexecing(destination)
+            logging.info("kernel loaded")
+            self._additionalDownload(destination)
+        self._sync()
+        after = time.time()
+        if self._talkToServer is not None:
+            self._talkToServer.done()
+        logging.info("Inaugurator took: %(interval).2fs. KEXECing", dict(interval=after - before))
+        self._loadKernel.execute()
+
+    def _assertArgsSane(self):
+        logging.info("Command line arguments: %(args)s", dict(args=self._args))
+        if self._args.inauguratorSource == "network":
+            assert (
+                (self._args.inauguratorServerAMQPURL or self._args.inauguratorNetworkLabel) and
+                self._args.inauguratorOsmosisObjectStores and
+                self._args.inauguratorUseNICWithMAC and self._args.inauguratorIPAddress and
+                self._args.inauguratorNetmask and self._args.inauguratorGateway), \
+                "If inauguratorSource is 'network', all network command line paramaters must be specified"
+            if self._args.inauguratorServerAMQPURL:
+                assert self._args.inauguratorMyIDForServer, \
+                    'If communicating with server, must specifiy --inauguratorMyIDForServer'
+        elif self._args.inauguratorSource == "DOK":
+            pass
+        elif self._args.inauguratorSource == "local":
+            pass
+        else:
+            assert False, "Unknown source for inaugurator: %s" % self._args.inauguratorSource
+
+    def _createPartitionTable(self):
+        lvmetad.Lvmetad()
+        partitionTable = partitiontable.PartitionTable(self._targetDevice)
+        if self._args.inauguratorClearDisk:
+            partitionTable.clear()
+        partitionTable.verify()
+
+    def _configureETC(self, destination):
+        self._etcLabelFile.write(self._label)
+        fstab.createFSTab(
+            rootPath=destination, root=self._mountOp.rootPartition(),
+            boot=self._mountOp.bootPartition(), swap=self._mountOp.swapPartition())
+        logging.info("/etc/fstab created")
+        if self._args.inauguratorChangeRootPassword:
+            passwd.setRootPassword(destination, self._args.inauguratorChangeRootPassword)
+            logging.info("Changed root password")
+
+    def _createBootAndInstallGrub(self, destination):
+        with self._mountOp.mountBoot() as bootDestination:
+            sh.run("rsync -rlpgDS --delete-before %s/boot/ %s/" % (destination, bootDestination))
+        with self._mountOp.mountBootInsideRoot():
+            logging.info("Installing grub")
+            grub.install(self._targetDevice, destination)
+
+    def _osmosFromNetwork(self, destination):
+        network.Network(
+            macAddress=self._args.inauguratorUseNICWithMAC, ipAddress=self._args.inauguratorIPAddress,
+            netmask=self._args.inauguratorNetmask, gateway=self._args.inauguratorGateway)
+        if self._args.inauguratorServerAMQPURL:
+            self._talkToServer = talktoserver.TalkToServer(
+                amqpURL=self._args.inauguratorServerAMQPURL, myID=self._args.inauguratorMyIDForServer)
+            self._talkToServer.checkIn()
+        osmos = osmose.Osmose(
+            destination=destination,
+            objectStores=self._args.inauguratorOsmosisObjectStores,
+            withLocalObjectStore=self._args.inauguratorWithLocalObjectStore,
+            ignoreDirs=self._args.inauguratorIgnoreDirs,
+            talkToServer=self._talkToServer)
+        if self._args.inauguratorServerAMQPURL:
+            self._label = self._talkToServer.label()
+        else:
+            self._label = self._args.inauguratorNetworkLabel
+        osmos.tellLabel(self._label)
+        osmos.wait()
+
+    def _osmosFromDOK(self, destination):
+        dok = diskonkey.DiskOnKey()
+        with dok.mount() as source:
+            osmos = osmose.Osmose(
+                destination, objectStores=source + "/osmosisobjectstore",
+                withLocalObjectStore=self._args.inauguratorWithLocalObjectStore,
+                ignoreDirs=self._args.inauguratorIgnoreDirs,
+                talkToServer=self._talkToServer)
+            with open("%s/inaugurate_label.txt" % source) as f:
+                self._label = f.read().strip()
+            osmos.tellLabel(self._label)  # This must stay under the dok mount 'with' statement
+            osmos.wait()
+
+    def _osmosFromLocalObjectStore(self, destination):
+        osmos = osmose.Osmose(
+            destination, objectStores=None,
+            withLocalObjectStore=self._args.inauguratorWithLocalObjectStore,
+            ignoreDirs=self._args.inauguratorIgnoreDirs,
+            talkToServer=self._talkToServer)
+        self._label = self._args.inauguratorNetworkLabel
+        osmos.tellLabel(self._label)
+        osmos.wait()
+
+    def _sync(self):
+        logging.info("sync...")
+        sh.run(["busybox", "sync"])
+        logging.info("sync done")
+
+    def _additionalDownload(self, destination):
+        if self._args.inauguratorDownload:
+            downloadInstance = download.Download(self._args.inauguratorDownload)
+            downloadInstance.download(destination)
+
+    def _makeSureDiskIsMountable(self):
+        udev.loadAllDrivers()
+        self._targetDevice = targetdevice.TargetDevice.device()
+        self._createPartitionTable()
+        logging.info("Partitions created")
+        self._mountOp = mount.Mount(self._targetDevice)
+
+    def _loadKernelForKexecing(self, destination):
+        self._loadKernel = loadkernel.LoadKernel()
+        self._loadKernel.fromBootPartitionGrubConfig(
+            bootPath=os.path.join(destination, "boot"), rootPartition=self._mountOp.rootPartition(),
+            append=self._args.inauguratorPassthrough)
+
+    def _doOsmosisFromSource(self, destination):
+        osmosiscleanup.OsmosisCleanup(destination)
+        if self._args.inauguratorSource == 'network':
+            self._osmosFromNetwork(destination)
+        elif self._args.inauguratorSource == 'DOK':
+            self._osmosFromDOK(destination)
+        elif self._args.inauguratorSource == 'local':
+            self._osmosFromLocalObjectStore(destination)
+        else:
+            assert False, "Unknown source %s" % self._args.inauguratorSource
--- inaugurator/main.py
+++ inaugurator/main.py
@@ -1,121 +1,13 @@
-from inaugurator import partitiontable
-from inaugurator import targetdevice
-from inaugurator import mount
-from inaugurator import sh
-from inaugurator import network
-from inaugurator import loadkernel
-from inaugurator import fstab
-from inaugurator import passwd
-from inaugurator import osmose
-from inaugurator import osmosiscleanup
-from inaugurator import talktoserver
-from inaugurator import grub
-from inaugurator import diskonkey
-from inaugurator import udev
-from inaugurator import download
-from inaugurator import etclabelfile
-from inaugurator import lvmetad
+from inaugurator import ceremony
 import argparse
 import traceback
 import pdb
-import os
-import time
 import logging
 import sys
 
 
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 logging.getLogger('pika').setLevel(logging.INFO)
-
-
-def main(args):
-    before = time.time()
-    udev.loadAllDrivers()
-    targetDevice = targetdevice.TargetDevice.device()
-    lvmetad.Lvmetad()
-    partitionTable = partitiontable.PartitionTable(targetDevice)
-    if args.inauguratorClearDisk:
-        partitionTable.clear()
-    partitionTable.verify()
-    logging.info("Partitions created")
-    mountOp = mount.Mount(targetDevice)
-    talkToServer = None
-    with mountOp.mountRoot() as destination:
-        etcLabelFile = etclabelfile.EtcLabelFile(destination)
-        osmosiscleanup.OsmosisCleanup(destination)
-        if args.inauguratorSource == 'network':
-            network.Network(
-                macAddress=args.inauguratorUseNICWithMAC, ipAddress=args.inauguratorIPAddress,
-                netmask=args.inauguratorNetmask, gateway=args.inauguratorGateway)
-            if args.inauguratorServerAMQPURL:
-                talkToServer = talktoserver.TalkToServer(
-                    amqpURL=args.inauguratorServerAMQPURL, myID=args.inauguratorMyIDForServer)
-                talkToServer.checkIn()
-            osmos = osmose.Osmose(
-                destination=destination,
-                objectStores=args.inauguratorOsmosisObjectStores,
-                withLocalObjectStore=args.inauguratorWithLocalObjectStore,
-                ignoreDirs=args.inauguratorIgnoreDirs,
-                talkToServer=talkToServer)
-            if args.inauguratorServerAMQPURL:
-                label = talkToServer.label()
-            else:
-                label = args.inauguratorNetworkLabel
-            osmos.tellLabel(label)
-            osmos.wait()
-        elif args.inauguratorSource == 'DOK':
-            dok = diskonkey.DiskOnKey()
-            with dok.mount() as source:
-                osmos = osmose.Osmose(
-                    destination, objectStores=source + "/osmosisobjectstore",
-                    withLocalObjectStore=args.inauguratorWithLocalObjectStore,
-                    ignoreDirs=args.inauguratorIgnoreDirs,
-                    talkToServer=talkToServer)
-                with open("%s/inaugurate_label.txt" % source) as f:
-                    label = f.read().strip()
-                osmos.tellLabel(label)  # This must stay under the dok mount 'with' statement
-                osmos.wait()
-        elif args.inauguratorSource == 'local':
-            osmos = osmose.Osmose(
-                destination, objectStores=None,
-                withLocalObjectStore=args.inauguratorWithLocalObjectStore,
-                ignoreDirs=args.inauguratorIgnoreDirs)
-            label = args.inauguratorNetworkLabel
-            osmos.tellLabel(label)
-            osmos.wait()
-        else:
-            assert False, "Unknown source %s" % args.inauguratorSource
-        print "Osmosis complete"
-        etcLabelFile.write(label)
-        with mountOp.mountBoot() as bootDestination:
-            sh.run("rsync -rlpgDS --delete-before %s/boot/ %s/" % (destination, bootDestination))
-        with mountOp.mountBootInsideRoot():
-            print "Installing grub"
-            grub.install(targetDevice, destination)
-        print "Boot sync complete"
-        fstab.createFSTab(
-            rootPath=destination, root=mountOp.rootPartition(),
-            boot=mountOp.bootPartition(), swap=mountOp.swapPartition())
-        print "/etc/fstab created"
-        if args.inauguratorChangeRootPassword:
-            passwd.setRootPassword(destination, args.inauguratorChangeRootPassword)
-            print "Changed root password"
-        loadKernel = loadkernel.LoadKernel()
-        loadKernel.fromBootPartitionGrubConfig(
-            bootPath=os.path.join(destination, "boot"), rootPartition=mountOp.rootPartition(),
-            append=args.inauguratorPassthrough)
-        print "kernel loaded"
-        if args.inauguratorDownload:
-            downloadInstance = download.Download(args.inauguratorDownload)
-            downloadInstance.download(destination)
-    print "sync..."
-    sh.run(["busybox", "sync"])
-    print "sync done"
-    after = time.time()
-    if talkToServer is not None:
-        talkToServer.done()
-    print "Inaugurator took: %.2fs. KEXECing" % (after - before)
-    loadKernel.execute()
 
 
 parser = argparse.ArgumentParser(add_help=False)
@@ -138,24 +30,8 @@ parser.add_argument("--inauguratorIgnoreDirs", nargs='+', default=[])
 try:
     cmdLine = open("/proc/cmdline").read().strip()
     args = parser.parse_known_args(cmdLine.split(' '))[0]
-    print "Command line arguments:", args
-    if args.inauguratorSource == "network":
-        assert (
-            (args.inauguratorServerAMQPURL or args.inauguratorNetworkLabel) and
-            args.inauguratorOsmosisObjectStores and
-            args.inauguratorUseNICWithMAC and args.inauguratorIPAddress and
-            args.inauguratorNetmask and args.inauguratorGateway), \
-            "If inauguratorSource is 'network', all network command line paramaters must be specified"
-        if args.inauguratorServerAMQPURL:
-            assert args.inauguratorMyIDForServer, \
-                'If communicating with server, must specifiy --inauguratorMyIDForServer'
-    elif args.inauguratorSource == "DOK":
-        pass
-    elif args.inauguratorSource == "local":
-        pass
-    else:
-        assert False, "Unknown source for inaugurator: %s" % args.inauguratorSource
-    main(args)
+    ceremonyInstance = ceremony.Ceremony(args)
+    ceremonyInstance.ceremony()
 except Exception as e:
     print "Inaugurator raised exception: "
     traceback.print_exc(e)
