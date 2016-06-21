2ee05ac1ba39473284471483855e014dabf73c6a
Alexander Solganik <sasha@lightbitslabs.com>
2016-06-21 23:19:26 +0300

    lab: fix lightbox make

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+source ${WORKSPACE_TOP}/common/tools/version_functions
 
 if [ "$1" != "inaugurator"  ]; then
 	exit 1
@@ -8,14 +8,5 @@ fi
 cd $WORKSPACE_TOP/inaugurator
 
 VERSION=$(git rev-parse HEAD)
-UNVERSIONED_FILES=$(git status -s  | awk '{print $2}')
-
-VERSION=$(md5sum <<< "${VERSION}")| awk '{print $1}'
-
-if [ x"$UNVERSIONED_FILES" != x"" ]; then 
-	HASHES="$(md5sum ${UNVERSIONED_FILES} |  awk '{print $1}')  $VERSION"
-	VERSION=$(md5sum <<< ${HASHES} | awk '{print $1}')
-fi
-
-echo $VERSION | cut -c1-12
-
+UNVERSIONED=$(md5sum_git_modified_unversioned .)
+echo $(md5sum_string "$VERSION$UNVERSIONED" | cut -c1-12)
