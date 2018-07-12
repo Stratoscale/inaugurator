7d3e95da524ee50ba88f40c9ee6c6ea5521b4971
Noam Chadash <noam@stratoscale.com>
2018-07-12 12:22:06 +0300

    Adding cciss as well to go to our initrd

--- sh/list_storage_and_network_driver_names.sh
+++ sh/list_storage_and_network_driver_names.sh
@@ -3,4 +3,4 @@
 BLACKLIST='-e bfa -e csiostor -e cxgb4 -e bna -e aic94xx'
 KERNEL_VERSION=$1
 set -e
-find /lib/modules/$KERNEL_VERSION/kernel/drivers/net/ethernet /lib/modules/$KERNEL_VERSION/kernel/drivers/scsi /lib/modules/$KERNEL_VERSION/kernel/drivers/nvme /lib/modules/$KERNEL_VERSION/kernel/drivers/message -type f -printf '%f\n' | sed 's/\.ko$//' | sed 's/\.ko.xz$//' | grep -v $BLACKLIST
+find /lib/modules/$KERNEL_VERSION/kernel/drivers/net/ethernet /lib/modules/$KERNEL_VERSION/kernel/drivers/scsi /lib/modules/$KERNEL_VERSION/kernel/drivers/nvme /lib/modules/$KERNEL_VERSION/kernel/drivers/message /lib/modules/$KERNEL_VERSION/kernel/weak-updates/cciss -type f -printf '%f\n' | sed 's/\.ko$//' | sed 's/\.ko.xz$//' | grep -v $BLACKLIST
