f27b0e7d139c9bc93599fc724b5f41373331e581
Guy Menahem <guymenahem@neokarm.com>
2020-05-26 14:45:07 +0300

    NK-33 - add Realtek driver to black list

--- sh/list_storage_and_network_driver_names.sh
+++ sh/list_storage_and_network_driver_names.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #list the drivers for the fat version
-BLACKLIST='-e bfa -e csiostor -e cxgb4 -e bna -e aic94xx'
+BLACKLIST='-e bfa -e csiostor -e cxgb4 -e bna -e aic94xx -e r8169'
 KERNEL_VERSION=$1
 set -e
 find /lib/modules/$KERNEL_VERSION/kernel/drivers/net/ethernet /lib/modules/$KERNEL_VERSION/kernel/drivers/scsi /lib/modules/$KERNEL_VERSION/kernel/drivers/nvme /lib/modules/$KERNEL_VERSION/kernel/drivers/message /lib/modules/$KERNEL_VERSION/kernel/weak-updates/cciss -type f -printf '%f\n' | sed 's/\.ko$//' | sed 's/\.ko.xz$//' | grep -v $BLACKLIST
