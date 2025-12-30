55418c16fc2d159192f5622a98459ba95e8eb2c3
Roei Goldenberg <roei.goldenberg@zadarastorage.com>
2025-12-30 23:17:41 +0200

    NK-17303 Allow mounting the EFI partition in the rootfs

--- inaugurator/fstab.py
+++ inaugurator/fstab.py
@@ -3,12 +3,20 @@ import os
 
 def createFSTab(rootPath, root, boot, swap):
     filename = os.path.join(rootPath, "etc", "fstab")
+    if os.path.exists('/sys/firmware/efi'):
+        _TEMPLATE = _COMMON_TEMPLATE + _EFI_TEMPLATE.stripe("\n") % _getEFIPartitionId()
+    else:
+        _TEMPLATE = _COMMON_TEMPLATE
     with open(filename, "w") as f:
-        f.write(_TEMPLATE % dict(root=root, boot=boot, swap=swap))
+        f.write(_TEMPLATE)
 
 
-_TEMPLATE = """
+_COMMON_TEMPLATE = """
 LABEL=ROOT /                       ext4    defaults        1 1
 LABEL=SWAP swap                    swap    defaults        0 0
 LABEL=BOOT /boot                   ext4    defaults        1 2
+"""
+
+_EFI_TEMPLATE="""
+LABEL=EFI /boot/efi                 vfat    umask=0077,shortname=winnt 0 2
 """
